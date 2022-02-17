90fa9423c7b39bc0eac2cb5d7c7ff2ed103f9253
UMenashe <menash2004@gmail.com>
2022-02-17 22:49:53 +0200

    create new start page

--- App.js
+++ App.js
@@ -1,11 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground,TouchableOpacity } from 'react-native';
+import { useEffect ,useState} from 'react';
+import Clock from './Clock';
+import img from './assets/instagramlogo.jpg';
+import img2 from './assets/youtube_social.png';
+import imgbg from './assets/backgroundG.jpeg';
+import imgw from './assets/whatsapp_icon.png';
 
 export default function App() {
+    const [timerDays, setTimerDays] = useState();
+    const [timerHours, setTimerHours] = useState();
+    const [timerMinutes, setTimerMinutes] = useState();
+    const [timerSeconds, setTimerSeconds] = useState();
+  
+    let interval;
+  
+    const startTimer = () => {
+      const countDownDate = new Date("March 6, 2022 16:00:00").getTime();
+  
+      interval = setInterval(() => {
+        const now = new Date().getTime();
+  
+        const distance = countDownDate - now;
+  
+        const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+        const hours = Math.floor((distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+        const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+  
+        if (distance < 0) {
+          clearInterval(interval);
+        } else {
+          setTimerDays(days);
+          setTimerHours(hours);
+          setTimerMinutes(minutes);
+          setTimerSeconds(seconds);
+        }
+      });
+    };
+  
+    useEffect(() => {
+      startTimer();
+    });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={imgbg} resizeMode="cover" style={styles.image}>
+      <View style={{marginVertical:20,alignItems:'center'}}>
+        <Text style={{fontSize:30,fontWeight:"bold",margin:20}}>הכתרה תשפ"ב</Text>
+        <Text style={{fontSize:18,margin:15}}>הספירה לאחור החלה!</Text>
+        <Clock
+        timerDays={timerDays}
+        timerHours={timerHours}
+        timerMinutes={timerMinutes}
+        timerSeconds={timerSeconds}
+      />
+      <View style={{marginTop:30}}>
+        <Text style={{fontSize:18,textAlign:"center",margin:20}}>מחזור י"א מזמין אתכם להכתרה תשפ"ב שתתקיים בתאריך:</Text>
+        <Text style={{fontSize:18,textAlign:"center",fontWeight:"bold",margin:10}}>ג' באדר ב' | 6.3 למניינם</Text>
+        <Text style={{fontSize:18,textAlign:"center",fontWeight:"bold",margin:10}}>כולם מוזמנים!</Text>
+        </View>
+      </View>
+      <View style={{marginVertical:20,alignItems:'center'}}>
+
+      <TouchableOpacity activeOpacity={0.7} style={styles.btn} onPress={(e)=> {window.open("https://wa.me/?text=The text to share!", "_blank");}}> 
+        <View style={{display:"flex",flexDirection:"row",justifyContent:"space-evenly",alignItems:"center"}}>
+        <img width="25" height="25" src={imgw}></img>
+        <Text style={{color:"#fff",fontWeight:"bold"}}>שתפו עם חברים</Text>
+        </View>
+        </TouchableOpacity>
+
+        <Text style={{fontSize:18}}>עקבו אחרינו</Text>
+        <View style={styles.socialShare}>
+         <a style={{backgroundColor:"#25D36",marginInline:15,color:"#fff"}} target="_blank" href='https://instagram.com/shminit_tzvia_katif' >
+         <img width="30" height="30" src={img}></img> 
+         </a>
+         <a style={{width:30,height:30,backgroundColor:"#25D36",marginRight:15,color:"#fff"}} target="_blank" href='https://youtube.com/channel/UCjYuosIDryUdGyl6QIuXJCw'>
+         <img width="30" height="30" src={img2}></img> 
+         </a>
+        </View>
+      </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -15,6 +90,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"space-between"
   },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  socialShare:{
+    display:"flex",
+    flexDirection:"row",
+    justifyContent:"space-between",
+    paddingVertical:7,
+    paddingHorizontal:5,
+    marginVertical:13,
+    marginHorizontal:7,
+    backgroundColor:"#00000030",
+    borderRadius:25,
+  },btn:{
+    width:"43%",
+    height:40,
+    backgroundColor:'#0466c8',
+    borderRadius:50,
+    textAlign:"center",
+    marginBottom:50,
+    justifyContent:"center",
+   }
 });
---
+++ Clock.js
@@ -0,0 +1,64 @@
+import React, { Fragment} from "react";
+import { StyleSheet, Text, View ,SectionList} from 'react-native';
+
+
+const Clock = ({ timerDays, timerHours, timerMinutes, timerSeconds }) => {
+  return (
+    <View>
+          <View style={styles.clock}>
+            <View style={styles.clockSec}>
+              <p style={{fontSize:23,fontWeight:"bold"}}>{timerDays}</p>
+              <small>ימים</small>
+            </View>
+            <span>:</span>
+            <View style={styles.clockSec}>
+              <p style={{fontSize:23,fontWeight:"bold"}}>{timerHours}</p>
+              <small>שעות</small>
+            </View>{" "}
+            <span>:</span>
+            <View style={styles.clockSec}>
+              <p style={{fontSize:23,fontWeight:"bold"}}>{timerMinutes}</p>
+              <small>דקות</small>
+            </View>{" "}
+            <span>:</span>
+            <View style={styles.clockSec}>
+              <p style={{fontSize:23,fontWeight:"bold"}}>{timerSeconds}</p>
+              <small>שניות</small>
+            </View>
+          </View>
+    </View>
+  );
+};
+
+Clock.defaultProps = {
+  timerDays: 10,
+  timerHours: 10,
+  timerMinutes: 10,
+  timerSeconds: 10,
+};
+
+const styles = StyleSheet.create({
+  clock: {
+    position: "relative",
+    margin: 15,
+    paddingHorizontal:15,
+    paddingBottom:10,
+    display: "flex",
+    flexDirection:"row",
+    justifyContent: "center",
+    alignItems: "center",
+    fontWeight:"bold",
+    color: "#00000",
+    backgroundColor:"#fff",
+    borderRadius: 15,
+    borderWidth:1 ,
+  },
+  clockSec:{
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+  }
+});
+export default Clock;
